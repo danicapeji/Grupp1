144bff11b3323990fbbb4de511e5b49b2a2363b9
frodinger <frej.hedlin@gmail.com>
2024-05-28 14:56:49 +0200

    testing
    bara testar lite

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 const LandingPage = () => {
     return (
-        <h1 className="font-serif text-blue-600">Home</h1>
+    <h1 className="font-serif text-blue-600">Home</h1>
     )
 }
 export default LandingPage;
