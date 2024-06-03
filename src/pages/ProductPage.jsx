eae4b7b7f6ee95d33450459bd1dcdee0e8ccc1f1
frodinger <frej.hedlin@gmail.com>
2024-06-03 15:20:24 +0200

    breadcrumbs

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { fetchProductById } from "../api";
 import { useCart } from "../context/CartContext";
-
-const ProductPage = ({ onAddToCart }) => {
+const ProductPage = () => {
   const { addToCart } = useCart();
   const [quantity, setQuantity] = useState(1);
-
-  const handleAddToCart = () => {
-    addToCart(product, quantity);
-  };
-
   const { id } = useParams();
+
   const {
     data: product,
     error,
@@ -21,31 +16,47 @@ const ProductPage = ({ onAddToCart }) => {
     queryKey: ["product", id],
     queryFn: () => fetchProductById(id),
   });
+
+  const handleAddToCart = () => {
+    addToCart(product, quantity);
+  };
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
+
   return (
-    <div className="flex flex-col md:flex-row w-full p-4 mt-20">
-      <div className='w-full h-[32rem] bg-white flex items-center justify-center'>
+    <div className='flex flex-col p-4 mt-16'>
+      <div className='flex text-black'>
+        <Link to="/">Home</Link>
+        <p>&nbsp;/&nbsp;</p>
+        <Link to="/shop">Shop</Link>
+        <p>&nbsp;/&nbsp;</p>
+        <Link to={`/product/${product.id}`}>{product.title}</Link>
+      </div>
+      <div className="flex flex-col md:flex-row mt-4">
+        <div className='w-full h-[32rem] bg-white flex items-center justify-center'>
           <img className='object-contain max-w-full max-h-full p-4' src={product.image} alt={product.title} />
         </div>
-      <div className='p-4'>
-        <h1>{product.title}</h1>
-        <p>${product.price}</p>
-        <p>{product.description}</p>
-        <div className="flex items-center">
-          <input
-            type="number"
-            value={quantity}
-            onChange={(e) => setQuantity(parseInt(e.target.value))}
-            className="border w-12 text-center mr-2 bg-white"
-            min="1"
-          />
-          <button onClick={handleAddToCart} className="bg-blue-500 text-white px-2 py-1">
-            Add to Cart
-          </button>
+        <div className='p-4'>
+          <h1>{product.title}</h1>
+          <p>${product.price}</p>
+          <p>{product.description}</p>
+          <div className="flex items-center">
+            <input
+              type="number"
+              value={quantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+              className="border w-12 text-center mr-2 bg-white"
+              min="1"
+            />
+            <button onClick={handleAddToCart} className="bg-blue-500 text-white px-2 py-1">
+              Add to Cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default ProductPage;
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { useQuery } from '@tanstack/react-query';
 import { fetchProducts } from '../api';
 import ProductCard from '../components/ProductCard';
@@ -13,11 +14,18 @@ const ShopPage = () => {
     if (error) return <div>Error: {error.message}</div>;
     
     return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-1 md:gap-4 mt-20 mb-20">
-        {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+        <section className='flex flex-col p-4 mt-16'>
+            <div className='flex text-black'>
+                <Link to="/">Home</Link>
+                <p>&nbsp;/&nbsp;</p>
+                <Link to="/shop">Shop</Link>
+            </div>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-1 md:gap-4 mt-4 mb-20">
+                {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+            </div>
+        </section>
     );
 };
 
