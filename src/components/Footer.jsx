69433c535c49a3d705fd9f187b853b5a1f591ff8
OlleLeonard <olleleonard@gmail.com>
2024-06-03 16:49:42 +0200

    Snygg fot

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 const Footer = () => {
   return (
-    <footer className="fixed w-full bg-blue-500 text-white py-8">
+    <footer className="fixed w-full bg-slate-500 text-white py-2">
       <div className="container flex flex-col md:flex-row justify-between items-center">
-        <div className="h-full w-full mb-4 md:mb-0 p-5">
+        <div className="h-full w-full mb-4 md:mb-0 p-1 pl-16">
           <h2 className="text-xl font-semibold">Kontakt</h2>
           <p>
             Email:{" "}
             <a
               href="mailto:Info@Hemsida.se"
-              className="underline bg-blue-500 text-white"
+              className="underline bg-slate-500 text-white"
             >
               Info@Hemsida.se
             </a>
@@ -17,7 +17,7 @@ const Footer = () => {
             Phone:{" "}
             <a
               href="tel:+46322650780"
-              className="underline bg-blue-500 text-white"
+              className="underline bg-slate-500 text-white"
             >
               +46 322 650 780
             </a>
@@ -25,41 +25,56 @@ const Footer = () => {
           <p>Address: Mattssons väg 4</p>
           <p>44192 Tokholm</p>
         </div>
-        <div className="container mx-auto text-center mt-4">
+        <div className="container flex mx-auto mx:justify-center md:text-center mt-4">
           <div className="flex flex-col md:flex-column text-center">
             <p className="mb-4">
               Follow us on social media and other platforms!
             </p>
-            <div className="flex justify-center space-x-2">
-              <button className="bg-white text-blue-500 p-3 m-2 rounded-full"></button>
-              <button className="bg-white text-blue-500 p-3 m-2 rounded-full"></button>
-              <button className="bg-white text-blue-500 p-3 m-2 rounded-full"></button>
-              <button className="bg-white text-blue-500 p-3 m-2 rounded-full"></button>
+            <div className="flex justify-center text-center">
+              <span className="[&>svg]:h-5 [&>svg]:w-5 m-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                </svg>
+              </span>
+              <span className="[&>svg]:h-5 [&>svg]:w-5 m-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                </svg>
+              </span>
+              <span className="[&>svg]:h-5 [&>svg]:w-5 m-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+                </svg>
+              </span>
+              <span className="[&>svg]:h-5 [&>svg]:w-5 m-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z" />
+                </svg>
+              </span>
             </div>
           </div>
         </div>
       </div>
+      <hr className="w-1/2 h-px bg-gray-200 border-0 dark:bg-gray-700 mx-auto" />
 
       <div className="container mx-auto text-center mt-4">
         <p className="text-white">&copy; 2021 All Rights Reserved</p>
-      </div>
-      <div className="container mx-auto text-center mt-4">
-        <button className="bg-white text-blue-500 p-2 rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M5 15l7-7 7 7"
-            />
-          </svg>
-        </button>
       </div>
     </footer>
   );
