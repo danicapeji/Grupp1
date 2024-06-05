d2168f1ae54c2638a990cee5efb5daf520954521
sasiie <Sarchiradic99@gmail.com>
2024-06-05 10:12:37 +0200

    små fix

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -17,26 +17,26 @@ const LandingPage = () => {
   return (
     <div className="header w-full p-4 mt-20">
       <h2 className="text-2xl font-semibold text-center">
-        Welcome to Our Store!
+        Välkommen!
       </h2>
       <p className="text-lg text-gray-700 mb-4 text-center">
-        Explore our latest collection and experience a world of style and
-        comfort!
+      Upptäck vår senaste kollektion och upplev en värld av stil och
+          komfort! Våra kläder är designade med både kvalitet och mode i åtanke,
+          så att du alltid kan känna dig självsäker och trendig.
       </p>
       <div
-        className="p-4 m-4 border border-gray-300 bg-gray-100 rounded-lg shadow-lg flex flex-col justify-start"
+        className="p-4 m-4 border border-gray-300 bg-gray-100 rounded-lg shadow-lg flex flex-col justify-end"
         style={{ height: "600px" }}
       >
         <p className="text-gray-700 text-center">
-          Upptäck vår senaste kollektion och upplev en värld av stil och
-          komfort! Våra kläder är designade med både kvalitet och mode i åtanke,
-          så att du alltid kan känna dig självsäker och trendig. Från eleganta
+        Från eleganta
           vardagsplagg till sofistikerade festoutfits – vi har något för varje
           tillfälle. Välkommen in och förnya din garderob med tidlösa favoriter
           och säsongens hetaste nyheter. Shoppa nu och hitta din nya look hos
           oss!
         </p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px- rounded">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px- rounded flex justify-center"
+        style={{ width: "200px" }}>
   Shoppa nu
 </button>
       </div>
