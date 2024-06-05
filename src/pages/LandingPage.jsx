4ac83772b1c904422de8f07c1836401a18f485fc
sasiie <Sarchiradic99@gmail.com>
2024-06-05 13:50:10 +0200

    tog bort svensk text

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -17,14 +17,7 @@ const LandingPage = () => {
   if (error) return <div>Error: {error.message}</div>;
   return (
     <div className="header w-full p-4 mt-20">
-  <h2 className="text-2xl font-semibold text-center">
-    Välkommen!
-  </h2>
-  <p className="text-lg text-gray-700 mb-4 text-center">
-    Upptäck vår senaste kollektion och upplev en värld av stil och
-    komfort! Våra kläder är designade med både kvalitet och mode i åtanke,
-    så att du alltid kan känna dig självsäker och trendig.
-  </p>
+
   <div
     className="relative p-4 m-4 border border-gray-300 rounded-lg shadow-lg flex flex-col justify-end"
     style={{
@@ -51,6 +44,14 @@ const LandingPage = () => {
     Explore our latest collection and experience a world of style and
     comfort!
   </p>
+  <div className="grid grid-cols-2 lg:grid-cols-4 gap-1 md:gap-4 mt-8">
+    {products.slice(0, 4).map((product) => (
+      <ProductCard key={product.id} product={product} />
+    ))}
+  </div>
+  <h2 className="text-2xl font-semibold text-center">
+    Explore our categories!
+  </h2>
   <div className="grid grid-cols-2 lg:grid-cols-4 gap-1 md:gap-4 mt-8">
     {products.slice(0, 4).map((product) => (
       <ProductCard key={product.id} product={product} />
