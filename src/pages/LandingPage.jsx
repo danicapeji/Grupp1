9553f085b4ae3bfc3c71d4c75741f793a8754666
frodinger <frej.hedlin@gmail.com>
2024-06-14 19:27:10 +0200

    min

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -20,7 +20,6 @@ const LandingPage = () => {
   if (categoriesError) return <div>Error: {categoriesError.message}</div>;
 
   return (
-<<<<<<< HEAD
     <div className="mt-[4rem] w-full"> {/* Adjust the margin-top based on your navbar height */}
       <section className="overflow-hidden bg-[url('https://images.unsplash.com/photo-1574634534894-89d7576c8259?q=80&w=3164&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-top bg-no-repeat">
         <div className="bg-black/25 p-8 md:p-12 lg:px-16 lg:py-24">
@@ -66,54 +65,6 @@ const LandingPage = () => {
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
-=======
-    <div className="header w-full p-4 mt-20">
-      <h2 className="text-2xl font-semibold text-center">Välkommen!</h2>
-      <p className="text-lg text-gray-700 mb-4 text-center">
-        Upptäck vår senaste kollektion och upplev en värld av stil och komfort!
-        Våra kläder är designade med både kvalitet och mode i åtanke, så att du alltid kan känna dig självsäker och trendig.
-      </p>
-      <div
-        className="relative p-4 m-4 border border-gray-300 rounded-lg shadow-lg flex flex-col justify-end"
-        style={{
-          height: "600px",
-          backgroundImage: `url('https://images.unsplash.com/photo-1574634534894-89d7576c8259?q=80&w=3164&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          color: 'white',
-        }}
-      >
-        <h2 className="absolute top-0 left-1/2 transform -translate-x-1/2 mt-4 text-2xl font-semibold text-center text-white">
-          Discover our latest collection and experience a world of style and comfort!
-        </h2>
-        <p className="absolute top-16 left-1/2 transform -translate-x-1/2 mt-4 text-center text-white max-w-xl">
-          Our clothes are designed with both quality and fashion in mind, so you can always feel confident and trendy.
-          From elegant everyday wear to sophisticated party outfits – we have something for every occasion. 
-        </p>
-        <Link to="/shop" className="mt-4 flex justify-center">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded">Shop now</button>
-        </Link>
-      </div>
-      <h2 className="text-2xl font-semibold text-center">Kategorier</h2>
-      <div>
-        {categories.map(category => (
-          <Link key={category} to={`/shop?category=${category}`}>
-            <button className="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
-              <div className="h-40 w-40 bg-gray-400"></div> {/* Placeholder för kategoribild */}
-              <p>{category}</p>
-            </button>
-          </Link>
-        ))}
-      </div>
-      <h2 className="text-2xl font-semibold text-center">Start here!</h2>
-      <p className="text-lg text-gray-700 mb-4 text-center">
-        Explore our latest collection and experience a world of style and comfort!
-      </p>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5 mt-8">
-        {products.slice(0, 4).map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
->>>>>>> parent of acff8c2 (Merge pull request #52 from danicapeji/Sara)
       </div>
     </div>
   );
