f23be69f9340d5429f87493f057b60ec78ebac5f
frodinger <63499844+frodinger@users.noreply.github.com>
2024-06-17 15:30:19 +0200

    Revert "Revert "Dev into main""

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchProducts, fetchCategories } from "../api";
+import ProductCard from "../components/ProductCard";
+import { Link } from "react-router-dom";
+
+const LandingPage = () => {
+  const {
+    data: products,
+    error: productsError,
+    isLoading: productsLoading,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: fetchProducts,
+  });
+
+  const {
+    data: categories,
+    error: categoriesError,
+    isLoading: categoriesLoading,
+  } = useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchCategories,
+  });
+
+  if (productsLoading || categoriesLoading) return <div>Loading...</div>;
+  if (productsError) return <div>Error: {productsError.message}</div>;
+  if (categoriesError) return <div>Error: {categoriesError.message}</div>;
+
+  // Function to find the first product image for each category
+  const getCategoryImages = () => {
+    const categoryImages = {};
+
+    categories.forEach((category) => {
+      const productInCategory = products.find(
+        (product) => product.category === category
+      );
+
+      if (productInCategory) {
+        categoryImages[category] = productInCategory.image;
+      } else {
+        categoryImages[category] = "default_image_url.jpg"; // Default image if no product found
+      }
+    });
+
+    return categoryImages;
+  };
+
+  const categoryImages = getCategoryImages();
+
+  return (
+    <div className="mt-[4rem] w-full">
+      {" "}
+      {/* Adjust the margin-top based on your navbar height */}
+      <section className="overflow-hidden bg-center bg-[url('https://images.unsplash.com/photo-1574634534894-89d7576c8259?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-top bg-no-repeat">
+        <div className="bg-black/25 p-4 py-8 md:p-8 lg:p-12 lg:px-16 lg:py-24">
+          <div className="flex flex-col justify-center">
+            <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl w-2/3">
+              Experience a world of style and comfort
+            </h2>
+            <p className="text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed w-4/5">
+              Our clothes are designed with both quality and fashion in mind, so
+              you can always feel confident and trendy. From elegant everyday
+              wear to sophisticated party outfits – we have something for every
+              occasion.
+            </p>
+            <div className="mt-4 sm:mt-8">
+              <Link
+                to="/shop"
+                className="py-3 px-5 rounded bg-blue-500 text-white hover:bg-blue-600"
+              >
+                Shop Now
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="container mx-auto p-4 lg:p-12 lg:px-16 lg:py-24">
+        <h2 className="text-2xl font-semibold">Categories</h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5 mt-4 mb-4">
+          {categories.map((category) => (
+            <Link
+              key={category}
+              to={`/shop?category=${category}`}
+              className="mx-2 mb-4"
+            >
+              <button className="bg-white shadow-lg text-black font-bold rounded-lg flex flex-col items-center w-full">
+                <div className="w-full h-60 lg:h-72 bg-white flex items-center justify-center rounded-t-lg overflow-hidden">
+                  <img
+                    src={categoryImages[category]}
+                    alt={category}
+                    className="object-contain max-w-full max-h-full p-2 hover:opacity-80"
+                  />
+                </div>
+                <p className="my-2 font-medium">
+                  {category.charAt(0).toUpperCase() +
+                    category.slice(1).toLowerCase()}
+                </p>
+              </button>
+            </Link>
+          ))}
+        </div>
+
+        <h2 className="text-2xl font-semibold">Start here!</h2>
+        <p className="text-lg text-gray-700 mb-4">
+          Explore our latest collection and experience a world of style and
+          comfort!
+        </p>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5 mt-8 mb-8">
+          {products.slice(0, 4).map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ src/pages/ShoppingCartPage.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+
+const CartPage = () => {
+  const { cart, updateQuantity, removeFromCart, clearCart } = useCart(); // Add clearCart
+  const navigate = useNavigate();
+  const [editingProductId, setEditingProductId] = useState(null);
+  const [newQuantity, setNewQuantity] = useState(1);
+
+  // Calculate the total price of the entire shopping cart
+  const totalPrice = cart.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
+
+  const handleCheckout = () => {
+    navigate("/order-confirmation", { state: { products: cart } });
+    clearCart(); // Clear the cart after navigating
+  };
+
+  const handleEditQuantity = (productId, quantity) => {
+    setEditingProductId(productId);
+    setNewQuantity(quantity);
+  };
+
+  const handleUpdateQuantity = (productId) => {
+    if (newQuantity > 0) {
+      updateQuantity(productId, newQuantity);
+      setEditingProductId(null);
+    }
+  };
+
+  return (
+    <div className="container flex mx-auto justify-center mt-10 h-fit">
+      {cart.length === 0 ? (
+        <div>
+          <p>Your cart is empty</p>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-6">
+            <Link to="/shop">Back to shop</Link>
+          </button>
+        </div>
+      ) : (
+        <div className="w-4/5 md:w-3/4 bg-white mt-20 p-4 md:p-8 rounded-lg shadow-lg">
+          <h1 className="text-2xl font-semibold mb-4 text-center">
+            Shopping Cart
+          </h1>
+          {cart.map((product) => (
+            <div key={product.id} className="border-b border-gray-300 py-2">
+              <div className="flex justify-items-start">
+                <div className="w-20 h-24 mr-4 bg-white flex items-center justify-center">
+                  <img
+                    className="object-contain max-w-full max-h-full p-1"
+                    src={product.image}
+                    alt={product.title}
+                  />
+                </div>
+                <div>
+                  <p className="font-semibold">{product.title}</p>
+                  <p>Price: ${product.price}</p>
+                  {editingProductId === product.id ? (
+                    <div className="flex">
+                      <button
+                        onClick={() =>
+                          setNewQuantity(newQuantity > 1 ? newQuantity - 1 : 1)
+                        }
+                        className="border rounded-l px-2 py-1 text-sm bg-gray-200 hover:bg-gray-300"
+                      >
+                        -
+                      </button>
+                      <input
+                        type="number"
+                        value={newQuantity}
+                        onChange={(e) =>
+                          setNewQuantity(parseInt(e.target.value))
+                        }
+                        className="border w-12 text-center bg-white py-1 text-sm"
+                        min="1"
+                      />
+                      <button
+                        onClick={() => seNewtQuantity(newQuantity + 1)}
+                        className="border rounded-r px-2 py-1 text-sm bg-gray-200 hover:bg-gray-300"
+                      >
+                        +
+                      </button>
+                      <button
+                        className="bg-green-500 hover:bg-green-600 text-white px-2 py-1 ml-2 rounded"
+                        onClick={() => handleUpdateQuantity(product.id)}
+                      >
+                        Update
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="flex items-center">
+                      <p>Quantity: {product.quantity}</p>
+                      <button
+                        className="rounded bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 ml-2"
+                        onClick={() =>
+                          handleEditQuantity(product.id, product.quantity)
+                        }
+                      >
+                        Edit
+                      </button>
+                    </div>
+                  )}
+                  <button
+                    className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 mt-2 rounded"
+                    onClick={() => removeFromCart(product.id)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+          <div className="mt-4 text-center">
+            <p className="font-semibold">
+              Total Price: ${totalPrice.toFixed(2)}
+            </p>
+            <button
+              onClick={handleCheckout}
+              className="rounded bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 mt-2"
+            >
+              Checkout
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CartPage;
